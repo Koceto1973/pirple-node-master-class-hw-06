a3ba3f2f7904e3d79a7bb59290c6143ef57413ef
TeamK <koceto1973@yahoo.ca>
2019-03-06 01:23:18 +0200

    app clustered to use available cores for api routing

--- index.js
+++ index.js
@@ -1,84 +1,26 @@
 // Dependencies
-var http = require('http');
-var https = require('https');
-var url = require('url');
-var StringDecoder = require('string_decoder').StringDecoder;
-var config = require('./config');
-var fs = require('fs');
+var server = require('./server');
+var cluster = require('cluster');
+var os = require('os');
 
-var unifiedServer = function(req,res){ // HTTP / HTTPS servers logic
+// Declare the app
+var app = {};
 
-    var parsedUrl = url.parse(req.url, true); // Parse the url
-  
-    var trimmedPath = parsedUrl.pathname.replace(/^\/+|\/+$/g, ''); // Get the trimmed path
-  
-    // Get the payload,if any
-    var decoder = new StringDecoder('utf-8');
-    var buffer = '';
+// Init function
+app.init = function(callback){
 
-    req.on('data', function(data) {
-        buffer += decoder.write(data);
-    });
+  // If we're on the master thread, just fork loop
+  if(cluster.isMaster){
 
-    req.on('end', function() {
-        buffer += decoder.end();
+    // Fork the process
+    for(var i = 0; i < os.cpus().length; i++){
+      cluster.fork();
+    }
 
-        var data = { // Construct the request data object 
-            'trimmedPath' : trimmedPath,
-            'queryStringObject' : parsedUrl.query, // Get the query string as an object
-            'method' : req.method.toLowerCase(), // Get the HTTP method
-            'headers' : req.headers, //Get the headers as an object
-            'payload' : buffer,
-          };
-
-        RoutesHandler(trimmedPath)(data,function(statusCode,payload){ // handle the request
-          
-          res.setHeader('Content-Type', 'application/json');
-          res.writeHead(typeof(statusCode) == 'number' ? statusCode : 200);
-          res.end(JSON.stringify(typeof(payload) == 'object'? payload : {}));
-          console.log(trimmedPath,statusCode);
-        });
-  
-    });
+  } else {
+    // If we're not on the master thread, start the HTTP server
+    server.init();
+  }
 };
 
- var httpServer = http.createServer(function(req,res){ // HTTP server up
-  unifiedServer(req,res);
-});
-
-httpServer.listen(config.httpPort,function(){ // HTTP server running
-  console.log('The HTTP server is running on port '+config.httpPort);
-});
-
-var httpsServerOptions = { // HTTPS server set up options
-  'key': fs.readFileSync('./https/key.pem'),
-  'cert': fs.readFileSync('./https/cert.pem')
-};
-
-var httpsServer = https.createServer(httpsServerOptions,function(req,res){ // HTTPS server up
-  unifiedServer(req,res);
-});
-
-httpsServer.listen(config.httpsPort,function(){ // HTTPS server running
- console.log('The HTTPS server is running on port '+config.httpsPort);
-});
-
-const RoutesHandler = function(path) {
-    switch (path) {
-        case 'ping' : // ping route
-          return function(data,callback){
-            callback(200);
-        };
-        case 'hello': // hello route
-          return function(data,callback){
-            callback(200, {
-                greetMsg: "Hi there, you are welcome to use our API!"
-            });
-        };
-        default     : // unknown routes
-          return function(data,callback){
-            callback(404);
-        };
-    };
-};
-
+app.init();
